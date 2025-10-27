4827c175271aef2a81e8e73f59f2b91724feb41c
dani4feed <dan.arbatov@ukr.net>
2025-10-27 15:11:07 +0200

    alpha 0.0.1 -chore: creation of barebones NextJS template

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <p>"Thus a board shall be(!)"</p>
+    <Button>So let it be.</Button>
+  </div>
+  );
+}
