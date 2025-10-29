30cf52ea64e39d2cf96c5476f380ae1b2447c422
dani4feed <dan.arbatov@ukr.net>
2025-10-29 18:27:14 +0200

    alpha 0.0.4 -feat: navbar and sidebar introduction

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,14 +2,11 @@ import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-y-4">
       <div>
         <p>"Thus authenticated shall user be(!)"</p>
-      </div>
-      <div>    
-        <UserButton/>
       </div>
     </div>
   );
