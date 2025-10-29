a709d7edaca95afd25dd7e7e1114f24aab1e537a
dani4feed <dan.arbatov@ukr.net>
2025-10-29 19:39:28 +0200

    alpha 0.0.5 -feat: Clerk organizations introduction

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,8 +1,13 @@
+import { List } from "./list";
+import { NewButton } from "./new-button";
+
 export const Sidebar = () => {
     return (
         <aside className="fixed z-[1] left-0
-         bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4">
-            The Side also can be here!
+         bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
+            Your Group can also be below
+            <List />
+            <NewButton />
         </aside>
     );
 };
