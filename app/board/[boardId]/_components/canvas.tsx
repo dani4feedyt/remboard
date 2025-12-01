1236c4335e00330fc3b3da78d8831c146e6c740f
Kapusta <ivankapusta212@gmail.com>
2025-12-02 00:33:16 +0200

    alpha 0.2.5 -feat

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -23,6 +23,7 @@ import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/
 import {nanoid} from "nanoid"
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 
 
@@ -88,6 +89,51 @@ const insertLayer = useMutation ((
       setMyPresence({selection:[layerId]}, {addToHistory: true})
       setCanvasState({mode:CanvasMode.None})
   },[lastUsedColor])
+
+
+
+  const translateSelectedLayers = useMutation((
+    {storage,self},
+    point : Point,
+  )=>{
+      if(canvasState.mode!==CanvasMode.Translating){
+        return
+      }
+
+      const offset = {
+        x:point.x - canvasState.current.x,
+        y:point.y - canvasState.current.y,
+      }
+
+
+      const liveLayers = storage.get('layers')
+
+      for(const id of self.presence.selection){
+        const layer = liveLayers.get(id)
+
+        if(layer){
+          layer.update({
+            x: layer.get("x") + offset.x,
+            y: layer.get("y") + offset.y,
+          })
+        }
+      }
+
+
+      setCanvasState({mode: CanvasMode.Translating,current:point})
+  },[
+    canvasState,
+  ])
+
+
+      const unselectLayers = useMutation ((
+        {self,setMyPresence}
+      )=>{
+        if (self.presence.selection.length>0){
+          setMyPresence({selection : []}, {addToHistory: true})
+        }
+
+      },[])
 
 
 
@@ -157,7 +203,10 @@ const insertLayer = useMutation ((
 
     const current = pointerEventToCanvasPoint(e, camera)
 
-    if(canvasState.mode === CanvasMode.Resizing) {
+
+    if(canvasState.mode === CanvasMode.Translating){
+        translateSelectedLayers(current)
+    } else if(canvasState.mode === CanvasMode.Resizing) {
       resizeSelectedLayer(current)
     }
 
@@ -166,6 +215,7 @@ const insertLayer = useMutation ((
     camera,
     canvasState,
     resizeSelectedLayer,
+    translateSelectedLayers,
   ])
 
 
@@ -176,13 +226,37 @@ const insertLayer = useMutation ((
       setMyPresence({cursor:null})
   },[])
 
+
+    const onPointerDown = useCallback((
+      e: React.PointerEvent,
+    )=>{
+        const point = pointerEventToCanvasPoint(e, camera)
+        if(canvasState.mode === CanvasMode.Inserting){
+          return
+        }
+
+        setCanvasState({origin: point,mode:CanvasMode.Pressing})
+    },[camera, canvasState.mode,setCanvasState])
+
+
+
   const onPointerUp = useMutation ((
     {},
     e
   )=>{
       const point = pointerEventToCanvasPoint(e,camera)
 
-      if(canvasState.mode===CanvasMode.Inserting){
+      if(
+        canvasState.mode === CanvasMode.None ||
+        canvasState.mode === CanvasMode.Pressing
+      ){
+          unselectLayers()
+
+
+        setCanvasState({
+          mode:CanvasMode.None
+        })
+      }else if(canvasState.mode===CanvasMode.Inserting){
         insertLayer(canvasState.layerType, point)
       }else{
         setCanvasState({
@@ -196,6 +270,7 @@ const insertLayer = useMutation ((
       canvasState,
       history,
       insertLayer,
+      unselectLayers,
   ])
 
   const selections = useOthersMapped((other) => other.presence.selection)
@@ -255,11 +330,16 @@ const insertLayer = useMutation ((
         undo={history.undo} 
         redo={history.redo}
         />
+        <SelectionTools
+        camera={camera}
+        setLastUsedColor={setLastUsedColor}
+        />
         <svg 
         className="h-screen w-screen" 
         onWheel={onWheel} 
         onPointerMove={onPointerMove} 
         onPointerLeave={onPointerLeave}
+        onPointerDown={onPointerDown}
         onPointerUp={onPointerUp}
         >
         <g style={{
---
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Color } from "@/types/canvas"
+
+interface ColorPickerProps {
+    onChange: (color:Color)=>void
+}
+
+
+export const ColorPicker = ({
+    onChange,
+}:ColorPickerProps) => {
+    return (
+        <div>
+            Color Picker!
+        </div>
+    )
+}
+
