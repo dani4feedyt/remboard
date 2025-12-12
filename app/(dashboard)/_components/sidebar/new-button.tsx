624160bd18af5af5f0756d27fddce22b52310592
dani4feed <dan.arbatov@ukr.net>
2025-12-12 13:41:13 +0200

    release 1.1.1 -fix: remade plus sign into the black one

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -20,7 +20,7 @@ export const NewButton = () => {
                 <button className="bg-white/25 h-full w-full
                 rounded-md flex items-center justify-center
                 opacity-60 hover:opacity-100 transition">
-                    <Plus className="text-white" />
+                    <Plus className="text-black" />
                 </button>
                 </Hint>
             </div>
